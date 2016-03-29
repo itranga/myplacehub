b0a0a0f203ebdb9acfd45919e3715d49fff9f918
Ranga Pathmasiri <rangapathmasiri@Rangas-MacBook-Air.local>
2016-03-30 05:18:28 +0530

    here we go

--- client/dist/server/config/db.conf.js
+++ client/dist/server/config/db.conf.js
@@ -5,8 +5,7 @@ var DBConfig = (function () {
     function DBConfig() {
     }
     DBConfig.init = function () {
-        var URL = (process.env.NODE_ENV === 'production') ? process.env.MONGOHQ_URL
-            : dbConst.localhost;
+        var URL = dbConst.localhost;
         mongoose.connect(URL);
         mongoose.connection.on('error', console.error.bind(console, 'An error ocurred with the DB connection: '));
     };
